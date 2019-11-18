6fa67030f5e1edc6310b57865254b37915fc62d8
David Burrell <53021181+dburrell32@users.noreply.github.com>
2019-11-18 17:37:54 +0100

    Create birth_weights.R

---
+++ birth_weights.R
@@ -0,0 +1,31 @@
+# Motivaing question:
+# Is there an observable difference in baby weight for smokers / non-smokers?
+
+# Install/library the `openintro` package
+# install.packages('openintro')
+# You'll also want to load dplyr and ggplot2 (as usual)
+
+# You should now be able to `View` the `births` dataset
+
+# Make an overlapping histogram of the birth weights of smokers/non-smokers
+# This is assessing the univariate distribution by a categorical variable
+
+# What are the mean birth weights for smokers/non-smokers
+
+# Is this difference statistically significant (to be answered via steps below)?
+
+# Pre-t-test conceptaul question: is this data paired?
+
+# Store weights from smokers/non-smokers in separate variables for easier access
+
+# Implement a t-test to assess the significance of this difference
+
+# Bonus: let's calculate the t value and confidence intervals manually
+
+# Compute the difference in means
+
+# Calculate the standard error
+
+# Compute the t score
+
+# Compute CIs using t score (you'll need to reference a t table)
